16b71851bb436630c1de2c985f38f3dbea35ee5e
sandeshtamang-cmd <np05cp4a240343@iic.edu.np>
2026-03-25 19:42:20 +0545

    chore: comment out EventSeeder and RegistrationSeeder in DatabaseSeeder

--- backend-laravel/database/seeders/DatabaseSeeder.php
+++ backend-laravel/database/seeders/DatabaseSeeder.php
@@ -10,8 +10,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            EventSeeder::class,
-            RegistrationSeeder::class,
+            // EventSeeder::class,
+            // RegistrationSeeder::class,
         ]);
     }
 }
